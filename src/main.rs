7ac28d5894b384f270e08a9a9c244436cb846f4f
monoflo <16790838+monoflo@users.noreply.github.com>
2022-03-18 20:16:08 -0400

    chore: clean-up test code from main

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,8 @@ use cell::Cell;
 use direction::Direction;
 
 fn main() {
-    println!("BOARD TEST");
     let mut board = Board::new();
-    board.shift(Direction::Up);
-    board.shift(Direction::Down);
-    board.shift(Direction::Left);
-    board.shift(Direction::Right);
-
     board.spawn().unwrap();
 
-    println!("{}", board.to_string());
-
-    println!("CELL TEST");
-
-    let mut cell = Cell::new();
-    println!("empty: {}", cell.to_string());
-
-    cell.spawn().unwrap();
-    println!("spawn: {}", cell.to_string());
-
-    cell.grow();
-    println!("grow: {}", cell.to_string());
-
-    cell.grow();
-    println!("grow: {}", cell.to_string());
+    print!("{}", board.to_string());
 }
