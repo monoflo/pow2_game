3dab5ce7f94c0601e8735b7dbcfc4278f0ef9ca5
monoflo <16790838+monoflo@users.noreply.github.com>
2022-03-23 23:03:51 -0400

    feat: establish user input loop, parsing
    - Create routine to parse user input into moves.
    - Establish REPL.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,39 @@ mod cell;
 mod coordinate;
 mod movement;
 
+use std::io::Write;
+
 use board::Board;
 use cell::Cell;
 use coordinate::Coordinate;
 use movement::Move;
 
+fn parse_input(inp: &str) -> Result<Move, ()> {
+    return match inp {
+        "w" => Ok(Move::Up),
+        "a" => Ok(Move::Left),
+        "s" => Ok(Move::Down),
+        "d" => Ok(Move::Right),
+        "u" => Ok(Move::Undo),
+        _ => Err(()),
+    };
+}
+
 fn main() {
+    let mut input = String::new();
     let mut board = Board::new();
 
-    print!("{}", board.to_string());
+    println!("{}", board.to_string());
+
+    loop {
+        print!("move: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input);
+        input.truncate(1);
+        let mut input = input.to_lowercase();
+        let mov: Move = parse_input(&input).expect("invalid move type");
+        let mut input = String::new();
+        board.movement(mov);
+        println!("{}", board.to_string());
+    }
 }
