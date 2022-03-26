54107266c15a50ae863bfd516a49c4bf0feab5af
monoflo <16790838+monoflo@users.noreply.github.com>
2022-03-25 21:49:37 -0400

    refactor: reimpl Cell as a tuple struct
    Felt unnecessary to keep using `value` everywhere. Keep it terse.

--- src/cell.rs
+++ src/cell.rs
@@ -3,27 +3,24 @@ use rand::Rng;
 /// Defines the likelihood of spawning a four instead of a two.
 const LIKELIHOOD_SPAWN_FOUR: f64 = 0.1;
 
-/// The representation of a cell on the game board.
 #[derive(Copy, Clone, Debug)]
-pub struct Cell {
-    /// The value represented by the cell.
-    value: usize,
-}
+/// The representation of a cell on the game board.
+pub struct Cell(usize);
 
 impl Cell {
     /// Returns a new, empty instance of a cell.
     pub fn new() -> Self {
-        Self { value: 0 }
+        Self(0)
     }
 
     /// Returns whether the cell is empty.
     pub fn is_empty(&self) -> bool {
-        self.value == 0
+        self.0 == 0
     }
 
     /// Returns the value held by the cell.
     pub fn value(&self) -> usize {
-        self.value
+        self.0
     }
 
     /// Merge the current cell with another.
@@ -34,7 +31,7 @@ impl Cell {
     ///
     /// * `other` - the other cell to merge with (that will be despawned)
     pub fn merge(&mut self, other: &mut Self) -> Result<(), ()> {
-        if self.is_empty() || self.value != other.value {
+        if self.is_empty() || self.0 != other.0 {
             return Err(());
         }
 
@@ -47,8 +44,8 @@ impl Cell {
     /// Attempts to spawn a non-empty cell.
     /// Fails if the cell already stores a non-zero value.
     pub fn spawn(&mut self) -> Result<(), ()> {
-        if self.value == 0 {
-            self.value = self.spawn_value();
+        if self.0 == 0 {
+            self.0 = self.spawn_value();
             return Ok(());
         }
         Err(())
@@ -66,23 +63,23 @@ impl Cell {
 
     /// Reverts the cell back to an empty state.
     fn despawn(&mut self) -> Result<(), ()> {
-        if self.value == 0 {
+        if self.0 == 0 {
             return Err(());
         }
-        self.value = 0;
+        self.0 = 0;
         Ok(())
     }
 
     /// Increases the value of the cell by a power of two.
     fn grow(&mut self) {
-        assert_ne!(0, self.value);
-        self.value <<= 1;
+        assert_ne!(0, self.0);
+        self.0 <<= 1;
     }
 }
 
 impl std::fmt::Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -91,24 +88,24 @@ mod tests {
     use super::*;
 
     #[test]
-    /// Affirm that `Cell::new()` initializes cells with `value = 0`.
+    /// Affirm that `Cell::new()` initializes cells with a zero value.
     fn new() {
         let cell = Cell::new();
-        assert_eq!(0, cell.value);
+        assert_eq!(0, cell.0);
     }
 
     #[test]
-    /// Affirm that `Cell::spawn()` can initialize the cell `value` to both two and four.
+    /// Affirm that `Cell::spawn()` can initialize the cell value to both two and four.
     fn spawn() {
         let mut cell = Cell::new();
         cell.spawn().unwrap();
 
         // test first value
-        let val = cell.value;
+        let val = cell.0;
         assert!(val == 2 || val == 4);
 
         // spawn until other value is reached
-        while val == cell.value {
+        while val == cell.0 {
             cell.despawn().unwrap();
             cell.spawn().unwrap();
         }
@@ -126,25 +123,25 @@ mod tests {
     }
 
     #[test]
-    /// Affirm that `Cell::grow()` will double the `value` of the cell.
+    /// Affirm that `Cell::grow()` will double the value of the cell.
     fn grow() {
-        let mut cell = Cell { value: 2 };
+        let mut cell = Cell(2);
         cell.grow();
-        assert_eq!(4, cell.value);
-        cell.value = 8;
+        assert_eq!(4, cell.0);
+        cell.0 = 8;
         cell.grow();
-        assert_eq!(16, cell.value);
-        cell.value = 32;
+        assert_eq!(16, cell.0);
+        cell.0 = 32;
         cell.grow();
-        assert_eq!(64, cell.value);
+        assert_eq!(64, cell.0);
     }
 
     #[test]
-    /// Affirm that `Cell::despawn()` will reset the `value` of the cell to zero.
+    /// Affirm that `Cell::despawn()` will reset the value of the cell to zero.
     fn despawn() {
-        let mut cell = Cell { value: 2 };
+        let mut cell = Cell(2);
         cell.despawn().unwrap();
-        assert_eq!(0, cell.value);
+        assert_eq!(0, cell.0);
     }
 
     #[test]
@@ -177,22 +174,22 @@ mod tests {
     }
 
     #[test]
-    /// Affirm that `Cell::merge()` will succeed when performed on two cells of equal `value`, and
-    /// also that their `value`s will update appropriately.
+    /// Affirm that `Cell::merge()` will succeed when performed on two cells of equal value, and
+    /// also that their values will update appropriately.
     fn merge_with_equal() {
-        let mut merger = Cell { value: 2 };
-        let mut mergee = Cell { value: 2 };
+        let mut merger = Cell(2);
+        let mut mergee = Cell(2);
         mergee.merge(&mut merger).unwrap();
-        assert_eq!(4, mergee.value);
-        assert_eq!(0, merger.value);
+        assert_eq!(4, mergee.0);
+        assert_eq!(0, merger.0);
     }
 
     #[test]
     /// Affirm that an error will occur if `Cell::merge()` is performed on two cells of unequal
-    /// `value`.
+    /// value.
     fn merge_with_unequal() {
-        let mut merger = Cell { value: 2 };
-        let mut mergee = Cell { value: 4 };
+        let mut merger = Cell(2);
+        let mut mergee = Cell(4);
         mergee.merge(&mut merger).unwrap_err();
     }
 }
